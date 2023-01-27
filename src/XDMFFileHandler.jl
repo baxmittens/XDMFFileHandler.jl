a435f6b44683e062af76c5419397e39cea234ca6
Admin.Bittens <Admin.Bittens@PC12392.pc.local>
2023-01-27 10:40:59 +0100

    ins

---
+++ src/XDMFFileHandler.jl
@@ -0,0 +1,9 @@
+using XMLParser
+
+interpolation_keywords = ["displacement","epsilon","pressure_interpolated","sigma","temperature_interpolated"]
+
+struct XDMF3File
+	name::String
+	xmlroot::XMLElement
+end
+
