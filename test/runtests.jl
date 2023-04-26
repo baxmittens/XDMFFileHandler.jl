9859374be8d3c68b5cde2ec7878324578cc0d151
Bittens <Bittens.M@bgr.local>
2023-04-26 17:49:15 +0200

    ins

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,5 @@ using XDMFFileHandler
 filename = "../test/test_THM_Ansicht_II_T1_grob_quadratic.xdmf"
 xdmffile = XDMF3File(filename)
 
-new = xdmffile*xdmffile
-write(xdmffile, "test.xdmf")
+newfile = xdmffile*xdmffile
+write(newfile, "test.xdmf")
