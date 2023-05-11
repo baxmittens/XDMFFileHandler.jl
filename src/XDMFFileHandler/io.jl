d7ba036ede3d57a705b2aa789e8b6dcb2a2d748d
Bittens <Bittens.M@bgr.local>
2023-05-11 16:03:14 +0200

    ins

--- src/XDMFFileHandler/io.jl
+++ src/XDMFFileHandler/io.jl
@@ -3,7 +3,6 @@ function create_and_update_hdf5!(xdmf3f::XDMF3File, newh5::String, newpath::Stri
 	_oldh5 = joinpath(path,oldh5)
 	_newh5 = joinpath(newpath,newh5)
 	xdmf3f.path = newpath
-	xdmf3f.h5file = newh5
 	cp(_oldh5,_newh5)
 	fid = h5open(_newh5,"r+")
 	for (name,field) in zip(xdmf3f.idata.names,xdmf3f.idata.fields)
@@ -11,25 +10,30 @@ function create_and_update_hdf5!(xdmf3f::XDMF3File, newh5::String, newpath::Stri
 		delete_object(fid, obj)
 		write(fid, obj, field.dat)
 	end
+	for dataitem in xdmf3f.dataitems
+		con = dataitem.content[1]
+		dataitem.content[1] = replace(con, xdmf3f.h5file=>newh5)
+	end
+	xdmf3f.h5file = newh5
 	close(fid)
 	return nothing
 end
 
-function correct_time_steps!(xdmf::XDMF3File)
-	h5file,xmlroot,path = xdmf.h5file,xdmf.xmlroot,xdmf.path
-	_h5file = joinpath(path,h5file)
-	fid = h5open(_h5file, "r")
-	times = read(fid, "times")
-	timeels = getElements(xmlroot, "Time")
-	for (t,el) in zip(times,timeels)
-		el.tag.attributes[1].val = string(t)
-	end
-	close(fid)
-	return nothing
-end
+#function correct_time_steps!(xdmf::XDMF3File)
+#	h5file,xmlroot,path = xdmf.h5file,xdmf.xmlroot,xdmf.path
+#	_h5file = joinpath(path,h5file)
+#	fid = h5open(_h5file, "r")
+#	times = read(fid, "times")
+#	timeels = getElements(xmlroot, "Time")
+#	for (t,el) in zip(times,timeels)
+#		el.tag.attributes[1].val = string(t)
+#	end
+#	close(fid)
+#	return nothing
+#end
 
 function update_xml!(xdmf3f::XDMF3File,newh5::String)
-	correct_time_steps!(xdmf3f)
+	#correct_time_steps!(xdmf3f)
 	for dataitem in xdmf3f.dataitems
 		con = dataitem.content[1]
 		dataitem.content[1] = replace(con, xdmf3f.h5file=>newh5)
@@ -42,6 +46,6 @@ function Base.write(xdmf3f::XDMF3File, name::String, path="./")
 	timest = timestamp()
 	newh5 = split(xdmf3f.h5file,".")[1]*timest*".h5"
 	create_and_update_hdf5!(xdmf3f, newh5, path)
-	update_xml!(xdmf3f,newh5)
+	#update_xml!(xdmf3f,newh5)
 	return write(joinpath(xdmf3f.path,name), xdmf3f.xmlfile)
 end
