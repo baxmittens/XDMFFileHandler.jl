1673fe6b3c0c5510bb8ad17f4a3b3a6ce41a86eb
Bittens <Bittens.M@bgr.local>
2023-06-07 16:44:24 +0200

    ins:wq

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,25 @@ xdmffile = XDMF3File(filename)
 
 newfile = xdmffile*xdmffile
 write(newfile, "test.xdmf")
+
+
+using Combinatorics
+top =  xdmffile.udata["topology"]
+combs = collect(combinations(1:4,3))
+
+faces = Vector{Vector{Int}}()
+
+for i in 1:8:length(top)
+	tet = top[i:i+3]
+	for comb in combs
+		push!(faces,tet[comb])
+	end
+end
+
+hullfaces = Vector{Vector{Int}}()
+sortedfaces = map(sort,faces)
+for (i,face) in enumerate(sortedfaces)
+	if length(findall(x->x==face,sortedfaces)) == 1
+		push!(hullfaces, faces[i])
+	end
+end
